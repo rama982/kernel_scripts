d5548e3c06cb42d94bca5fbef26be7f6d8eded5c
Rama Bondan Prakoso <ramanarubp@gmail.com>
2018-12-27 11:39:13 +0700

    Update local script

--- vince-miui.sh
+++ vince-miui.sh
@@ -1,47 +1,39 @@
-#!/bin/bash
-
-# Copyright (C) Abubakar Yagob (abubakaryagob@gmail.com)
-# Copyright (C) Rama Bondan Prakoso (ramnarubp@gmail.com)
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#!/usr/bin/env bash
+# Copyright (C) 2018 Abubakar Yagob (blacksuan19)
+# Copyright (C) 2018 Rama Bondan Prakoso (rama982)
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 # Color
 green='\033[0;32m'
+echo -e "$green"
 
-# Directories
+# Main Environment
 KERNEL_DIR=$PWD
 KERN_IMG=$KERNEL_DIR/out_vince-oreo-caf/arch/arm64/boot/Image.gz-dtb
 ZIP_DIR=$KERNEL_DIR/AnyKernel2
 CONFIG_DIR=$KERNEL_DIR/arch/arm64/configs
-
-# Move to kernel directory
-cd $KERNEL_DIR
-git clone https://github.com/rama982/AnyKernel2
-
-# Exports
-export ARCH=arm64
-export SUBARCH=arm64
-export PATH=/usr/lib/ccache:$PATH
-
-# Misc
 CONFIG=vince_defconfig
 CORES=$(grep -c ^processor /proc/cpuinfo)
 THREAD="-j$CORES"
+CROSS_COMPILE+="ccache "
+CROSS_COMPILE+="$PWD/stock/bin/aarch64-linux-android-"
 
-# Here We Go
-echo -e "$green---------------------------------------------------------------------";
+# Modules environtment
+OUTDIR="$PWD/out/"
+SRCDIR="$PWD/"
+MODULEDIR="$PWD/AnyKernel2/modules/system/lib/modules/"
+PRONTO=${MODULEDIR}pronto/pronto_wlan.ko
+STRIP="$PWD/stock/bin/$(echo "$(find "$PWD/stock/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
+			sed -e 's/gcc/strip/')"
+
+# Export
+export ARCH=arm64
+export SUBARCH=arm64
+export PATH=/usr/lib/ccache:$PATH
+export CROSS_COMPILE
+
+# Is this logo
+echo -e "---------------------------------------------------------------------";
 echo -e "---------------------------------------------------------------------\n";
 echo -e "   _____ ______ _   _  ____  __  __   _  ________ _____  _   _ ______ _      ";
 echo -e "  / ____|  ____| \ | |/ __ \|  \/  | | |/ /  ____|  __ \| \ | |  ____| |     ";
@@ -53,118 +45,127 @@ echo -e "---------------------------------------------------------------------";
 
 # Main script
 while true; do
-echo -e "\n$green[1] Build Vince MIUI Kernel (with Google GCC)"
-echo -e "[2] Regenerate defconfig"
-echo -e "[3] Source cleanup"
-echo -e "[4] Create flashable zip"
-echo -e "$red[5] Quit$nc"
-echo -ne "\n$brown(i) Please enter a choice[1-6]:$nc "
+	echo -e "\n[1] Build Vince MIUI Kernel"
+	echo -e "[2] Regenerate defconfig"
+	echo -e "[3] Source cleanup"
+	echo -e "[4] Create flashable zip"
+	echo -e "[5] Quit"
+	echo -ne "\n(i) Please enter a choice[1-6]: "
+	
+	read choice
+	
+	if [ "$choice" == "1" ]; then
+		echo -e "\n(i) Cloning AnyKernel2 if folder not exist..."
+		git clone https://github.com/rama982/AnyKernel2 -b vince-miui
+	
+		echo -e "\n(i) Cloning toolcahins if folder not exist..."
+		git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 --depth=1 stock 
+	
+		echo -e ""
+		make  O=out $CONFIG $THREAD &>/dev/null
+		make  O=out $THREAD & pid=$!   
+	
+		BUILD_START=$(date +"%s")
+		DATE=`date`
 
-read choice
+		echo -e "\n#######################################################################"
 
-if [ "$choice" == "1" ]; then
-echo -e "\n$green Cloning toolcahins if not exist..."
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 --depth=1 stock  
-echo -e "\n$green building with Google GCC..."
-CROSS_COMPILE+="ccache "
-CROSS_COMPILE+="$PWD/stock/bin/aarch64-linux-android-"
-export CROSS_COMPILE
-make  O=out $CONFIG $THREAD
-make  O=out $THREAD & pid=$!   
+		echo -e "(i) Build started at $DATE using $CORES thread"
 
-BUILD_START=$(date +"%s")
-DATE=`date`
-echo -e "\n$cyan#######################################################################$nc"
-echo -e "$brown(i) Build started at $DATE using $CORES thread$nc"
-  spin[0]="$blue-"
-  spin[1]="\\"
-  spin[2]="|"
-  spin[3]="/$nc"
-  echo -ne "\n$blue[Please wait...] ${spin[0]}$nc"
-  while kill -0 $pid &>/dev/null
-  do
-    for i in "${spin[@]}"
-    do
-          echo -ne "\b$i"
-          sleep 0.1
-    done
-  done
+		spin[0]="-"
+		spin[1]="\\"
+		spin[2]="|"
+		spin[3]="/"
+		echo -ne "\n[Please wait...] ${spin[0]}"
+		while kill -0 $pid &>/dev/null
+		do
+			for i in "${spin[@]}"
+			do
+				echo -ne "\b$i"
+				sleep 0.1
+			done
+		done
+	
+		if ! [ -a $KERN_IMG ]; then
+			echo -e "\n(!) Kernel compilation failed, See buildlog to fix errors"
+			echo -e "#######################################################################"
+			exit 1
+		fi
+	
+		BUILD_END=$(date +"%s")
+		DIFF=$(($BUILD_END - $BUILD_START))
 
-  if ! [ -a $KERN_IMG ]; then
-    echo -e "\n$green(!) Kernel compilation failed, See buildlog to fix errors $nc"
-    echo -e "$green#######################################################################$nc"
-    exit 1
-  fi
+		echo -e "\n(i) Image-dtb compiled successfully."
 
-  $DTBTOOL -2 -o $KERNEL_DIR/arch/arm/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/ &>/dev/null &>/dev/null
+		echo -e "#######################################################################"
 
-  BUILD_END=$(date +"%s")
-  DIFF=$(($BUILD_END - $BUILD_START))
-  echo -e "\n$brown(i)Image-dtb compiled successfully.$nc"
-  echo -e "$green#######################################################################$nc"
-  echo -e "$green(i) Total time elapsed: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nc"
-  echo -e "$green#######################################################################$nc"
-fi
+		echo -e "(i) Total time elapsed: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
 
-if [ "$choice" == "2" ]; then
-  echo -e "\n$cyan#######################################################################$nc"
-  make O=out  $CONFIG savedefconfig
-  cp out/.config arch/arm64/configs/vince-full_defconfig
-  cp out/defconfig arch/arm64/configs/$CONFIG
-  echo -e "$purple(i) Defconfig generated.$nc"
-  echo -e "$cyan#######################################################################$nc"
-fi
+		echo -e "#######################################################################"
+	fi
+	
+	if [ "$choice" == "2" ]; then
+		echo -e "\n#######################################################################"
 
-if [ "$choice" == "3" ]; then
-  echo -e "\n$cyan#######################################################################$nc"
-  rm -f $DT_IMG
-  make O=out clean &>/dev/null
-  make mrproper &>/dev/null
-  rm -rf out/*
-  echo -e "$purple(i) Kernel source cleaned up.$nc"
-  echo -e "$cyan#######################################################################$nc"
-fi
+		make O=out  $CONFIG savedefconfig &>/dev/null
+		cp out/.config arch/arm64/configs/sakura-full_defconfig &>/dev/null
+		cp out/defconfig arch/arm64/configs/$CONFIG &>/dev/null
 
+		echo -e "(i) Defconfig generated."
 
-if [ "$choice" == "4" ]; then
-echo -e "\n$green#######################################################################$nc"
-echo -e "\n$green Strip and move miui modules to AnyKernel2..."
-# thanks to @adekmaulana
-  cd $ZIP_DIR
-  make clean &>/dev/null
-  cd ..
-  OUTDIR="$PWD/out/"
-  SRCDIR="$PWD/"
-  MODULEDIR="$PWD/Zipper/modules/system/lib/modules/"
-  PRONTO=${MODULEDIR}pronto/pronto_wlan.ko
+		echo -e "#######################################################################"
+	fi
+	
+	if [ "$choice" == "3" ]; then
+		echo -e "\n#######################################################################"
 
-  STRIP="$PWD/stock/bin/$(echo "$(find "$PWD/stock/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
-sed -e 's/gcc/strip/')"
+		make O=out clean &>/dev/null
+		make mrproper &>/dev/null
+		rm -rf out/*
 
-  for MOD in $(find "${OUTDIR}" -name '*.ko') ; do
-      "${STRIP}" --strip-unneeded --strip-debug "${MOD}" &> /dev/null
-      "${SRCDIR}"/scripts/sign-file sha512 \
-                                  "${OUTDIR}/signing_key.priv" \
-                                  "${OUTDIR}/signing_key.x509" \
-                                  "${MOD}"
-      find "${OUTDIR}" -name '*.ko' -exec cp {} "${MODULEDIR}" \;
-      case ${MOD} in
+		echo -e "(i) Kernel source cleaned up."
 
-           */wlan.ko)
-             cp -ar "${MOD}" "${PRONTO}"
+		echo -e "#######################################################################"
+	fi
+	
+	if [ "$choice" == "4" ]; then
+		echo -e "\n#######################################################################"
 
-      esac
-  done
-  cd $ZIP_DIR
-  cp $KERN_IMG $ZIP_DIR/zImage
-  make normal &>/dev/null
-  cd ..
-  echo -e "$green(i) Flashable zip generated under $ZIP_DIR.$nc"
-  echo -e "$green#######################################################################$nc"
-fi
+		echo -e "\n(i) Strip and move modules to AnyKernel2..."
 
+		# thanks to @adekmaulana
 
-if [ "$choice" == "5" ]; then
- exit 
-fi
+		cd $ZIP_DIR
+		make clean &>/dev/null
+		cd ..
+	
+		for MOD in $(find "${OUTDIR}" -name '*.ko') ; do
+			"${STRIP}" --strip-unneeded --strip-debug "${MOD}" &> /dev/null
+			"${SRCDIR}"/scripts/sign-file sha512 \
+					"${OUTDIR}/signing_key.priv" \
+					"${OUTDIR}/signing_key.x509" \
+					"${MOD}"
+			find "${OUTDIR}" -name '*.ko' -exec cp {} "${MODULEDIR}" \;
+			case ${MOD} in
+				*/wlan.ko)
+					cp -ar "${MOD}" "${PRONTO}"
+			esac
+		done
+		echo -e "\n(i) Done moving modules"
+
+		cd $ZIP_DIR
+		cp $KERN_IMG $ZIP_DIR/zImage
+		make normal &>/dev/null
+		cd ..
+
+		echo -e "(i) Flashable zip generated under $ZIP_DIR."
+
+		echo -e "#######################################################################"
+	fi
+	
+	if [ "$choice" == "5" ]; then
+		exit 
+	fi
+
 done
+echo -e "$nc"
