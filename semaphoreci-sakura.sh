9cfebdceab5b27a5ce9523ca7619a0b4ca57dccc
Rama Bondan Prakoso <ramanarubp@gmail.com>
2018-12-27 12:02:30 +0700

    Update ci script

--- semaphoreci-sakura.sh
+++ semaphoreci-sakura.sh
@@ -1,27 +1,8 @@
 #!/usr/bin/env bash
+# SemaphoreCI Kernel Build Script
 # Copyright (C) 2018 Raphiel Rollerscaperers (raphielscape)
 # Copyright (C) 2018 Rama Bondan Prakoso (rama982)
 # SPDX-License-Identifier: GPL-3.0-or-later
-
-# Directories and package
-KERNEL_DIR=${HOME}/android_kernel_xiaomi_sakura-caf
-KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz
-ZIP_DIR=$KERNEL_DIR/AnyKernel2
-DTB=$KERNEL_DIR/out/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3.dtb
-
-# Detect which branch we now
-branch="$(git rev-parse --abbrev-ref HEAD)"
-
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 --depth=1 stock
-
-install-package --update-new ccache bc bash git-core gnupg build-essential \
-	zip curl make automake autogen autoconf autotools-dev libtool shtool python \
-	m4 gcc libtool zlib1g-dev
-		
-
-# Move to kernel directory
-cd $KERNEL_DIR
-git clone https://github.com/rama982/AnyKernel2 -b sakura-miui
 
 #
 # Telegram FUNCTION begin
@@ -36,11 +17,11 @@ TELEGRAM_TOKEN=${BOT_API_KEY}
 
 export TELEGRAM_TOKEN
 
-# Push to Channel
+# Push kernel installer to channel
 function push() {
-    JIP=$(echo Genom*.zip)
-    curl -F document=@$JIP  "https://api.telegram.org/bot$BOT_API_KEY/sendDocument" \
-	 -F chat_id="$TELEGRAM_ID"
+	JIP=$(echo Genom*.zip)
+	curl -F document=@$JIP  "https://api.telegram.org/bot$BOT_API_KEY/sendDocument" \
+			-F chat_id="$TELEGRAM_ID"
 }
 
 # Send the info up
@@ -53,47 +34,66 @@ function tg_channelcast() {
 		)"
 }
 
-
 function tg_sendinfo() {
-    curl -s "https://api.telegram.org/bot$BOT_API_KEY/sendMessage" \
-         -d "parse_mode=markdown" \
-         -d text="${1}" \
-         -d chat_id="$TELEGRAM_ID" \
-         -d "disable_web_page_preview=true"
+	curl -s "https://api.telegram.org/bot$BOT_API_KEY/sendMessage" \
+		-d "parse_mode=markdown" \
+		-d text="${1}" \
+		-d chat_id="$TELEGRAM_ID" \
+		-d "disable_web_page_preview=true"
 }
 
-# Errored Prober
+# Errored prober
 function finerr() {
-    tg_sendinfo "$(echo -e "Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds\nbut it's error...")"
-    exit 1
+	tg_sendinfo "$(echo -e "Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds\nbut it's error...")"
+	exit 1
 }
 
-
+# Send sticker
 function tg_sendstick() {
-    curl -s -X POST "https://api.telegram.org/bot$BOT_API_KEY/sendSticker" \
-         -d sticker="CAADBQADWAUAAs_rwQcJSj1jS_tMZwI" \
-         -d chat_id="$TELEGRAM_ID" >> /dev/null
+	curl -s -X POST "https://api.telegram.org/bot$BOT_API_KEY/sendSticker" \
+		-d sticker="CAADBQADgQADuMZ9GebdeJ3qOSmSAg" \
+		-d chat_id="$TELEGRAM_ID" >> /dev/null
 }
 
-
-# Announce the completion
-function tg_yay() {
-    tg_channelcast "Seem like compilation completed." \
-		"Uploading installer file..."
- }
-
-# Fin Prober
+# Fin prober
 function fin() {
-     tg_sendinfo "$(echo "Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.")"
-     tg_channelcast "<b>Note for Sakura user: If stuck on boootlogo after installing kernel flash Magisk</b>"
-
+	tg_sendinfo "$(echo "Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.")"
 }
 
 #
 # Telegram FUNCTION end
 #
 
-# Main script
+# Main environtment
+KERNEL_DIR=${HOME}/android_kernel_xiaomi_msm8953
+KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
+ZIP_DIR=$KERNEL_DIR/AnyKernel2
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+CONFIG=sakura_defconfig
+CORES=$(grep -c ^processor /proc/cpuinfo)
+THREAD="-j$CORES"
+CROSS_COMPILE+="ccache "
+CROSS_COMPILE+="$PWD/stock/bin/aarch64-linux-android-"
+
+# Export
+export JOBS="$(grep -c '^processor' /proc/cpuinfo)"
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER="ramakun"
+export CROSS_COMPILE
+
+# Install build package
+install-package --update-new ccache bc bash git-core gnupg build-essential \
+	zip curl make automake autogen autoconf autotools-dev libtool shtool python \
+	m4 gcc libtool zlib1g-dev
+
+# Clone toolchain
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 --depth=1 stock
+
+# Clone AnyKernel2
+git clone https://github.com/rama982/AnyKernel2 -b sakura-miui
+
+# Build start
 DATE=`date`
 BUILD_START=$(date +"%s")
 
@@ -106,18 +106,6 @@ tg_channelcast "<b>GENOM MIUI</b> kernel new build!" \
 	"Under commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>" \
 	"Started on <code>$(date)</code>"
 
-export JOBS="$(grep -c '^processor' /proc/cpuinfo)"
-
-export ARCH=arm64
-export SUBARCH=arm64
-export KBUILD_BUILD_USER="ramakun"
-CONFIG=sakura_defconfig
-CORES=$(grep -c ^processor /proc/cpuinfo)
-THREAD="-j$CORES"
-CROSS_COMPILE+="ccache "
-CROSS_COMPILE+="$PWD/stock/bin/aarch64-linux-android-"
-export CROSS_COMPILE
-
 make  O=out $CONFIG $THREAD
 make  O=out $THREAD
 
@@ -125,50 +113,46 @@ BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
 
 if ! [ -a $KERN_IMG ]; then
-    echo -e "Kernel compilation failed, See buildlog to fix errors"
-    finerr
-    exit 1
-else
-  tg_yay
-  cd $ZIP_DIR
-  make clean &>/dev/null
-  cd ..
-# for miui
-  OUTDIR="$PWD/out/"
-  SRCDIR="$PWD/"
-  MODULEDIR="$PWD/AnyKernel2/modules/system/lib/modules/"
-  PRIMA="$PWD/AnyKernel2/modules/system/vendor/lib/modules/wlan.ko"
-  PRONTO="$PWD/AnyKernel2/modules/system/vendor/lib/modules/pronto/pronto_wlan.ko"
-
-  STRIP="$PWD/stock/bin/$(echo "$(find "$PWD/stock/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
-sed -e 's/gcc/strip/')"
-
-  for MOD in $(find "${OUTDIR}" -name '*.ko') ; do
-      "${STRIP}" --strip-unneeded --strip-debug "${MOD}" &> /dev/null
-      "${SRCDIR}"/scripts/sign-file sha512 \
-                                  "${OUTDIR}/signing_key.priv" \
-                                  "${OUTDIR}/signing_key.x509" \
-                                  "${MOD}"
-      find "${OUTDIR}" -name '*.ko' -exec cp {} "${MODULEDIR}" \;
-      case ${MOD} in
-
-           */wlan.ko)
-             cp -ar "${MOD}" "${PRIMA}"
-             cp -ar "${MOD}" "${PRONTO}"
-
-      esac
-  done
-  rm $PWD/AnyKernel2/modules/system/lib/modules/wlan.ko
-  cd $ZIP_DIR
-  cp $KERN_IMG $ZIP_DIR/kernel/Image.gz
-  cp $DTB $ZIP_DIR/treble/msm8953-qrd-sku3-sakura.dtb
-  make normal &>/dev/null
-  echo Genom*.zip
-  echo -e "$purple(i) Flashable zip generated under $ZIP_DIR."
-  push
-  cd ..
-  fin
+	echo -e "Kernel compilation failed, See buildlog to fix errors"
+	finerr
+	exit 1
 fi
 
+cd $ZIP_DIR
+make clean &>/dev/null
+cd ..
 
+# For MIUI Build
+# Credit @adekmaulana
+OUTDIR="$PWD/out/"
+SRCDIR="$PWD/"
+MODULEDIR="$PWD/AnyKernel2/modules/system/lib/modules/"
+PRIMA="$PWD/AnyKernel2/modules/system/vendor/lib/modules/wlan.ko"
+PRONTO="$PWD/AnyKernel2/modules/system/vendor/lib/modules/pronto/pronto_wlan.ko"
+STRIP="$PWD/stock/bin/$(echo "$(find "$PWD/stock/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
+sed -e 's/gcc/strip/')"
 
+for MOD in $(find "${OUTDIR}" -name '*.ko') ; do
+	"${STRIP}" --strip-unneeded --strip-debug "${MOD}" &> /dev/null
+	"${SRCDIR}"/scripts/sign-file sha512 \
+			"${OUTDIR}/signing_key.priv" \
+			"${OUTDIR}/signing_key.x509" \
+			"${MOD}"
+	find "${OUTDIR}" -name '*.ko' -exec cp {} "${MODULEDIR}" \;
+	case ${MOD} in
+		*/wlan.ko)
+		cp -ar "${MOD}" "${PRIMA}"
+		cp -ar "${MOD}" "${PRONTO}"
+	esac
+	rm $PWD/AnyKernel2/modules/system/lib/modules/wlan.ko
+done
+
+cd $ZIP_DIR
+cp $KERN_IMG $ZIP_DIR/zImage
+make normal &>/dev/null
+echo Genom*.zip
+echo "Flashable zip generated under $ZIP_DIR."
+push
+cd ..
+fin
+# Build end
--- semaphoreci-vince.sh
+++ semaphoreci-vince.sh
@@ -1,33 +1,8 @@
 #!/usr/bin/env bash
+# SemaphoreCI Kernel Build Script
 # Copyright (C) 2018 Raphiel Rollerscaperers (raphielscape)
 # Copyright (C) 2018 Rama Bondan Prakoso (rama982)
 # SPDX-License-Identifier: GPL-3.0-or-later
-
-# Directories and package
-KERNEL_DIR=${HOME}/android_kernel_xiaomi_msm8953
-KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
-ZIP_DIR=$KERNEL_DIR/AnyKernel2
-
-# Detect which branch we now
-branch="$(git rev-parse --abbrev-ref HEAD)"
-
-# Clone toolchain
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 --depth=1 stock
-
-# Install build package
-install-package --update-new ccache bc bash git-core gnupg build-essential \
-	zip curl make automake autogen autoconf autotools-dev libtool shtool python \
-	m4 gcc libtool zlib1g-dev
-		
-# Move to kernel directory
-cd $KERNEL_DIR
-
-# Clone AnyKernel2
-if [ "$branch" = "vince/oreo-eas-miui" ]; then
-	git clone https://github.com/rama982/AnyKernel2 -b vince-miui
-else
-	git clone https://github.com/rama982/AnyKernel2 -b vince-aosp
-fi
 
 #
 # Telegram FUNCTION begin
@@ -35,18 +10,18 @@ fi
 
 git clone https://github.com/fabianonline/telegram.sh telegram
 
-TELEGRAM_ID=-1001479956662
+TELEGRAM_ID=-1001232319637
 
 TELEGRAM=telegram/telegram
 TELEGRAM_TOKEN=${BOT_API_KEY}
 
 export TELEGRAM_TOKEN
 
-# Push to Channel
+# Push kernel installer to channel
 function push() {
-    JIP=$(echo Genom*.zip)
-    curl -F document=@$JIP  "https://api.telegram.org/bot$BOT_API_KEY/sendDocument" \
-	 -F chat_id="$TELEGRAM_ID"
+	JIP=$(echo Genom*.zip)
+	curl -F document=@$JIP  "https://api.telegram.org/bot$BOT_API_KEY/sendDocument" \
+			-F chat_id="$TELEGRAM_ID"
 }
 
 # Send the info up
@@ -60,14 +35,14 @@ function tg_channelcast() {
 }
 
 function tg_sendinfo() {
-    curl -s "https://api.telegram.org/bot$BOT_API_KEY/sendMessage" \
-         -d "parse_mode=markdown" \
-         -d text="${1}" \
-         -d chat_id="$TELEGRAM_ID" \
-         -d "disable_web_page_preview=true"
+	curl -s "https://api.telegram.org/bot$BOT_API_KEY/sendMessage" \
+		-d "parse_mode=markdown" \
+		-d text="${1}" \
+		-d chat_id="$TELEGRAM_ID" \
+		-d "disable_web_page_preview=true"
 }
 
-# Errored Prober
+# Errored prober
 function finerr() {
 	tg_sendinfo "$(echo -e "Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds\nbut it's error...")"
 	exit 1
@@ -80,7 +55,7 @@ function tg_sendstick() {
 		-d chat_id="$TELEGRAM_ID" >> /dev/null
 }
 
-# Fin Prober
+# Fin prober
 function fin() {
 	tg_sendinfo "$(echo "Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.")"
 }
@@ -89,39 +64,60 @@ function fin() {
 # Telegram FUNCTION end
 #
 
-# Main script
-DATE=`date`
-BUILD_START=$(date +"%s")
-
-tg_sendstick
-
-if [ "$branch" = "vince/oreo-eas-miui" ]; then
-tg_channelcast "<b>GENOM MIUI</b> kernel new build!" \
-	"Started on <code>$(hostname)</code>" \
-	"For device <b>VINCE</b> (Redmi 5 Plus)" \
-	"At branch <code>${branch}</code>" \
-	"Under commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>" \
-	"Started on <code>$(date)</code>"
-else
-tg_channelcast "<b>GENOM CAF</b> (AOSP) kernel new build!" \
-	"Started on <code>$(hostname)</code>" \
-	"For device <b>VINCE</b> (Redmi 5 Plus)" \
-	"At branch <code>${branch}</code>" \
-	"Under commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>" \
-	"Started on <code>$(date)</code>"
-fi
-
-export JOBS="$(grep -c '^processor' /proc/cpuinfo)"
-
-export ARCH=arm64
-export SUBARCH=arm64
-export KBUILD_BUILD_USER="ramakun"
+# Main environtment
+KERNEL_DIR=${HOME}/android_kernel_xiaomi_msm8953
+KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
+ZIP_DIR=$KERNEL_DIR/AnyKernel2
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 CONFIG=vince_defconfig
 CORES=$(grep -c ^processor /proc/cpuinfo)
 THREAD="-j$CORES"
 CROSS_COMPILE+="ccache "
 CROSS_COMPILE+="$PWD/stock/bin/aarch64-linux-android-"
+
+# Export
+export JOBS="$(grep -c '^processor' /proc/cpuinfo)"
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER="ramakun"
 export CROSS_COMPILE
+
+# Install build package
+install-package --update-new ccache bc bash git-core gnupg build-essential \
+	zip curl make automake autogen autoconf autotools-dev libtool shtool python \
+	m4 gcc libtool zlib1g-dev
+
+# Clone toolchain
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 --depth=1 stock
+
+# Clone AnyKernel2
+if [ "$BRANCH" = "vince/oreo-eas-miui" ]; then
+	git clone https://github.com/rama982/AnyKernel2 -b vince-miui
+else
+	git clone https://github.com/rama982/AnyKernel2 -b vince-aosp
+fi
+
+# Build start
+DATE=`date`
+BUILD_START=$(date +"%s")
+
+tg_sendstick
+
+if [ "$BRANCH" = "vince/oreo-eas-miui" ]; then
+	tg_channelcast "<b>GENOM MIUI</b> kernel new build!" \
+		"Started on <code>$(hostname)</code>" \
+		"For device <b>VINCE</b> (Redmi 5 Plus)" \
+		"At branch <code>${BRANCH}</code>" \
+		"Under commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>" \
+		"Started on <code>$(date)</code>"
+else
+	tg_channelcast "<b>GENOM CAF</b> (AOSP) kernel new build!" \
+		"Started on <code>$(hostname)</code>" \
+		"For device <b>VINCE</b> (Redmi 5 Plus)" \
+		"At branch <code>${BRANCH}</code>" \
+		"Under commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>" \
+		"Started on <code>$(date)</code>"
+fi
 
 make  O=out $CONFIG $THREAD
 make  O=out $THREAD
@@ -130,44 +126,45 @@ BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
 
 if ! [ -a $KERN_IMG ]; then
-    echo -e "Kernel compilation failed, See buildlog to fix errors"
-    finerr
-    exit 1
-else
-  cd $ZIP_DIR
-  make clean &>/dev/null
-  cd ..
-# for miui
-  if [ "$branch" = "vince/oreo-eas-miui" ]; then
-  OUTDIR="$PWD/out/"
-  SRCDIR="$PWD/"
-  MODULEDIR="$PWD/AnyKernel2/modules/system/lib/modules/"
-  PRONTO=${MODULEDIR}pronto/pronto_wlan.ko
+	echo -e "Kernel compilation failed, See buildlog to fix errors"
+	finerr
+	exit 1
+fi
 
-  STRIP="$PWD/stock/bin/$(echo "$(find "$PWD/stock/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
+cd $ZIP_DIR
+make clean &>/dev/null
+cd ..
+
+# For MIUI Build
+# Credit @adekmaulana
+if [ "$BRANCH" = "vince/oreo-eas-miui" ]; then
+	OUTDIR="$PWD/out/"
+	SRCDIR="$PWD/"
+	MODULEDIR="$PWD/AnyKernel2/modules/system/lib/modules/"
+	PRONTO=${MODULEDIR}pronto/pronto_wlan.ko
+	STRIP="$PWD/stock/bin/$(echo "$(find "$PWD/stock/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
 sed -e 's/gcc/strip/')"
 
-  for MOD in $(find "${OUTDIR}" -name '*.ko') ; do
-      "${STRIP}" --strip-unneeded --strip-debug "${MOD}" &> /dev/null
-      "${SRCDIR}"/scripts/sign-file sha512 \
-                                  "${OUTDIR}/signing_key.priv" \
-                                  "${OUTDIR}/signing_key.x509" \
-                                  "${MOD}"
-      find "${OUTDIR}" -name '*.ko' -exec cp {} "${MODULEDIR}" \;
-      case ${MOD} in
-
-           */wlan.ko)
-             cp -ar "${MOD}" "${PRONTO}"
-
-      esac
-  done
-  fi
-  cd $ZIP_DIR
-  cp $KERN_IMG $ZIP_DIR/zImage
-  make normal &>/dev/null
-  echo Genom*.zip
-  echo "Flashable zip generated under $ZIP_DIR."
-  push
-  cd ..
-  fin
+	for MOD in $(find "${OUTDIR}" -name '*.ko') ; do
+		"${STRIP}" --strip-unneeded --strip-debug "${MOD}" &> /dev/null
+		"${SRCDIR}"/scripts/sign-file sha512 \
+				"${OUTDIR}/signing_key.priv" \
+				"${OUTDIR}/signing_key.x509" \
+				"${MOD}"
+		find "${OUTDIR}" -name '*.ko' -exec cp {} "${MODULEDIR}" \;
+		case ${MOD} in
+			*/wlan.ko)
+			cp -ar "${MOD}" "${PRONTO}"
+		esac
+	done
 fi
+
+cd $ZIP_DIR
+cp $KERN_IMG $ZIP_DIR/zImage
+make normal &>/dev/null
+echo Genom*.zip
+echo "Flashable zip generated under $ZIP_DIR."
+push
+cd ..
+fin
+# Build end
