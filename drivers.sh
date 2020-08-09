6b4f1f5f1d9eaceb2aba5c1970badc9c28070c83
Rama Bondan Prakoso <ramanarubp@gmail.com>
2020-08-09 13:25:11 +0700

    drivers: Rework

--- drivers.sh
+++ drivers.sh
@@ -1,73 +1,70 @@
-#!/usr/bin/env bash
-#
+#!/bin/bash
 # Copyright (C) 2019 Rama Bondan Prakoso (rama982)
-#
 # Scripts to merge / upstream kernel drivers (wifi and audio)
-#
 # for msm-4.4+
-#
-# Kanged from gist.github.com/nathanchance/072a16428f592b76e7f025ccd1bb01ae
 
 while (( ${#} )); do
-    case ${1} in
-        "-a"|"--audio") AUDIO=true ;;
-        "-i"|"--init") INIT=true ;;
-        "-p"|"--prima") PRIMA=true ;;
-        "-t"|"--tag") shift; TAG=${1} ;;
-        "-u"|"--update") UPDATE=true ;;
-    esac
-    shift
+  case ${1} in
+       "-a"|"--audio") AUDIO=true ;;
+       "-i"|"--init") INIT=true ;;
+       "-w"|"--wlan") WLAN=true ;;
+       "-p"|"--prima") PRIMA=true ;;
+       "-d"|"--data") DATA=true ;;
+       "-t"|"--tag") shift; TAG=${1} ;;
+       "-u"|"--update") UPDATE=true ;;
+  esac
+  shift
 done
 
 [[ -n ${INIT} && -n ${UPDATE} ]] && { echo "Both init and update were specified!"; exit; }
 
 [[ -z ${TAG} ]] && { echo "No tag was specified!"; exit; }
 
-SUBFOLDER_WLAN=drivers/staging
-if [[ -z ${PRIMA} ]]; then
+function drivers()
+{
+  if [[ $4 == "wlan" ]]; then
+     PREFIX="staging"
+     NAME="$1/$3"
+  else
+     PREFIX=techpack
+     NAME=$1
+  fi
+  echo "${3}"
+  git fetch "$2/$3" "${TAG}"
+  if [[ -n ${INIT} ]]; then
+    git subtree add --prefix="${NAME}" FETCH_HEAD
+    [[ $? -eq 1 ]] && { exit; }
+    git commit --amen -m "${PREFIX}: ${3}: Add from ${TAG}" -s
+  elif [[ -n ${UPDATE} ]]; then
+    git merge --no-edit -m "${PREFIX}: ${3}: Merge tag '${TAG}' into $(git rev-parse --abbrev-ref HEAD)"  \
+              -m "$(git log --oneline --no-merges $(git branch | grep "\*" | sed 's/\* //')..FETCH_HEAD)" \
+              -X subtree="${NAME}" --signoff FETCH_HEAD
+  fi
+}
+
+if [[ -n ${WLAN} ]]; then
+  SUBFOLDER_WLAN=drivers/staging
+  URL_WLAN=https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan
+  if [[ -z ${PRIMA} ]]; then
     REPOS_WLAN=( "fw-api" "qcacld-3.0" "qca-wifi-host-cmn" )
-else
+  else
     REPOS_WLAN=( "prima" )
+  fi
+  for REPO in "${REPOS_WLAN[@]}"; do
+    drivers $SUBFOLDER_WLAN $URL_WLAN $REPO wlan
+  done
 fi
 
-URL_WLAN=https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/
+if [[ -n ${AUDIO} ]]; then
+  SUBFOLDER_AUDIO=techpack/audio
+  REPO_AUDIO=( "audio-kernel" )
+  URL_AUDIO=https://source.codeaurora.org/quic/la/platform/vendor/opensource
+  drivers $SUBFOLDER_AUDIO $URL_AUDIO $REPO_AUDIO
+fi
 
-for REPO in "${REPOS_WLAN[@]}"; do
-    echo "${REPO}"
-    if ! git ls-remote --exit-code "${REPO}" &>/dev/null; then
-        git remote add "${REPO}" "${URL_WLAN}${REPO}"
-    fi
-    git fetch "${REPO}" "${TAG}"
-    if [[ -n ${INIT} ]]; then
-        git merge --allow-unrelated-histories -s ours --no-commit FETCH_HEAD
-        git read-tree --prefix="${SUBFOLDER_WLAN}/${REPO}" -u FETCH_HEAD
-        git commit --no-edit -m "staging: ${REPO}: Checkout at ${TAG}" -s
-    elif [[ -n ${UPDATE} ]]; then
-        git merge --no-edit -m "staging: ${REPO}: Merge tag '${TAG}' into $(git rev-parse --abbrev-ref HEAD)"  \
-                  -m "$(git log --oneline --no-merges $(git branch | grep "\*" | sed 's/\* //')..FETCH_HEAD)" \
-                  -X subtree="${SUBFOLDER_WLAN}/${REPO}" --signoff FETCH_HEAD --signoff
-    fi
-done
-
-[[ -z ${AUDIO} ]] && { exit; }
-
-SUBFOLDER_AUDIO=techpack/audio
-REPOS_AUDIO=( "audio-kernel" )
-URL_AUDIO=https://source.codeaurora.org/quic/la/platform/vendor/opensource/
-
-for REPO in "${REPOS_AUDIO[@]}"; do
-    echo "${REPO}"
-    if ! git ls-remote --exit-code "${REPO}" &>/dev/null; then
-        git remote add "${REPO}" "${URL_AUDIO}${REPO}"
-    fi
-    git fetch "${REPO}" "${TAG}"
-    if [[ -n ${INIT} ]]; then
-        git merge --allow-unrelated-histories -s ours --no-commit FETCH_HEAD
-        git read-tree --prefix="${SUBFOLDER_AUDIO}" -u FETCH_HEAD
-        git commit --no-edit -m "techpack: ${REPO}: Checkout at ${TAG}" -s
-    elif [[ -n ${UPDATE} ]]; then
-        git merge --no-edit -m "techpack: ${REPO}: Merge tag '${TAG}' into $(git rev-parse --abbrev-ref HEAD)" \
-                  -m "$(git log --oneline --no-merges $(git branch | grep "\*" | sed 's/\* //')..FETCH_HEAD)" \
-                  -X subtree="${SUBFOLDER_AUDIO}" FETCH_HEAD --signoff
-    fi
-done
+if [[ -n ${DATA} ]]; then
+  SUBFOLDER_DATA=techpack/data
+  REPO_DATA=( "data-kernel" )
+  URL_DATA=https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource
+  drivers $SUBFOLDER_DATA $URL_DATA $REPO_DATA
+fi
